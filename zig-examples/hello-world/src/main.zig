b1fe3565d49be92b003734a3a234c21b3d1c1b20
Tom Cheng <thomascheng1998@googlemail.com>
2019-10-06 16:45:16 +0100

    Added basic world setup + system dispatching

--- zig-examples/hello-world/src/main.zig
+++ zig-examples/hello-world/src/main.zig
@@ -1,8 +1,27 @@
 const ecs = @import("flecs");
 const std = @import("std");
 
-pub fn main() void {
+const Pos = struct {
+    x: f32, y: f32,
+};
+
+pub fn move(rows: ecs.Rows) void {
+}
+
+pub fn main() !void {
     std.debug.warn("Hello\n");
-    const world = ecs.c_flecs.ecs_init();
+    var world = ecs.World.init(std.debug.global_allocator);
+    defer world.deinit();
+    world.setTargetFps(60);
+    const cpos = world.registerComponent(Pos);
+    try world.registerSystem(c"move", ecs.Phase.OnUpdate, move, c"Pos");
+
+    const e = world.new();
+    world.set(e, cpos, Pos { .x = 0.0, .y = 0.0 });
+
+    while (true) {
+        world.progress();
+    }
+
 }
 
--- zig-src/flecs.zig
+++ zig-src/flecs.zig
@@ -1,2 +1,104 @@
 const std = @import("std");
 pub const c_flecs = @import("c_flecs.zig");
+
+pub const Entity = c_flecs.ecs_entity_t;
+
+pub const Component = struct {
+    inner: c_flecs.ecs_entity_t,
+};
+
+pub const Phase = enum(@TagType(c_flecs.EcsSystemKind)) {
+    OnUpdate = @enumToInt(c_flecs.EcsSystemKind.EcsOnUpdate),
+};
+
+pub const Rows = struct {
+    inner: *c_flecs.ecs_rows_t,
+    fn init(rows: *c_flecs.ecs_rows_t) @This() {
+        return @This() {
+            .inner = rows,
+        };
+    }
+
+    pub fn getSystemId(self: @This()) Entity {
+        return self.inner.system;
+    }
+};
+
+
+/// Map system entity IDs to zig functions
+const SystemMap = std.AutoHashMap(c_flecs.ecs_entity_t, fn(Rows) void);
+var system_map: ?SystemMap = null;
+
+extern "c" fn systemDispatcher(rows: [*c]c_flecs.ecs_rows_t) void {
+    // Wrap the rows
+    const wrapped = Rows.init(rows);
+
+    // Figure out which system this should be, dispatch that
+    var res_fn = system_map.?.get(rows.*.system);
+    if (res_fn) |function| { 
+        function.value(wrapped);
+    } else {
+        std.debug.warn("Error: can't find system {}\n", rows.*.system);
+    }
+}
+
+pub const World = struct {
+    inner: *c_flecs.ecs_world,
+    allocator: *std.mem.Allocator,
+    frame_delta: f32 = 1.0 / 60.0,
+
+    pub fn init(allocator: *std.mem.Allocator) @This() {
+        if (system_map == null) {
+            system_map = SystemMap.init(allocator);
+        } else {
+            @panic("Multiple worlds unsupported (for now).");
+        }
+        return @This() {
+            .inner = c_flecs.ecs_init().?,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: @This()) void {
+        _ = c_flecs.ecs_fini(self.inner);
+    }
+
+    pub fn setTargetFps(self: *@This(), fps: f32) void {
+        self.frame_delta = 1.0 / fps;
+        c_flecs.ecs_set_target_fps(self.inner, fps);
+    }
+
+    pub fn registerComponent(self: *@This(), comptime T: type) Component {
+        comptime var c_string_lit : [@typeName(T).len + 1]u8 = undefined;
+        comptime {
+            inline for (@typeName(T)) |c, ix| {
+                c_string_lit[ix] = c;
+            }
+            c_string_lit[c_string_lit.len - 1] = 0;
+        }
+        return Component { .inner = c_flecs.ecs_new_component(self.inner, &c_string_lit, @sizeOf(T)) };
+    }
+
+    pub fn progress(self: *@This()) void {
+        _ = c_flecs.ecs_progress(self.inner, self.frame_delta);
+    }
+
+    /// Register a system with the given signature: https://github.com/SanderMertens/flecs/blob/master/Manual.md#system-signatures
+    pub fn registerSystem(self: *@This(), name: [*c]const u8, phase: Phase, function: fn(Rows) void, sig: [*c]const u8) !void {
+        const sys = c_flecs.ecs_new_system(self.inner, name, @bitCast(c_flecs.EcsSystemKind, phase), sig, systemDispatcher);
+        _ = try system_map.?.put(sys, function);
+    }
+
+    pub fn new(self: *@This()) Entity {
+        return c_flecs._ecs_new(self.inner, null);
+    }
+
+    pub fn set(self: *@This(), entity: Entity, c_type: Component, val: var) void {
+        var val_copied = val;
+        _ = c_flecs._ecs_set_ptr(self.inner, entity, c_type.inner, @sizeOf(@typeOf(val)), &val_copied);
+    }
+};
+
+
+
+
